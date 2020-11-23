7c15d017283334814db84caa77b2308cae779b8e
Andrea Diaz <andrea.diaz.12@hotmail.com>
2020-11-23 20:53:24 +0000

    hi

--- backend/server.cpp
+++ backend/server.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <served/served.hpp>
 
+#include "data_processor.cpp"
+#include "porter_stemming.cpp"
+
+
 int main (int aargc, const char *argv[]) {
+	data_processor process_data("stopwords.txt");
+
+	process_data.add("data.txt");
+	process_data.add("data1.txt");
+	process_data.add("data2.txt");
+
+
 	served::multiplexer mux;
-    mux.handle("/api/v1/greeting")
+    mux.handle("/api/get-list")
         .get([&](served::response &res, const served::request &req) {
             std::string name = req.query["name"];
             res.set_header("content-type", "application/json");
@@ -13,7 +24,7 @@ int main (int aargc, const char *argv[]) {
     std::cout << "Try this example with:" << std::endl;
     std::cout << "  curl \"http://localhost:8123/api/v1/greeting?name=world\"" << std::endl;
 
-    served::net::server server("127.0.0.1", "8123", mux);
+    served::net::server server("0.0.0.0", "8081", mux);
     server.run(10);
 
     return (1);
